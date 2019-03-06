06fc40a6d801aa239ec2f166d7aacb9312f31a04
federicoCassettai <f.cassettai@gmail.com>
2019-03-06 11:38:23 +0100

    fixed bug login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -80,7 +80,7 @@ router.post("/signup", (req, res, next) => {
         res.json(userSaved);
       });
     })
-    .catch(err => next(err));
+    .catch(err => res.json(err));
 });
 
 router.post("/login", (req, res, next) => {
